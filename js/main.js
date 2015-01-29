5dcd4ea1b734312f61716aa3aa391de6659b1d9e
zachrussell <zrussell1@harding.edu>
2015-01-29 11:18:41 -0600

    Added textbox

--- js/main.js
+++ js/main.js
@@ -1,8 +1,28 @@
 $(document).ready(function(){
-	$("#similarPercentage").slider();
-	$("#redPercentage").slider();
-	$("#bluePercentage").slider();
-	$("#emptyPercentage").slider();
-	$("#boardSizePercentage").slider();
-	$("#delayTime").slider();
+
+	$("#similarPercentageText").val($("#similarPercentage").val());
+	$("#redPercentageText").val($("#redPercentage").val());
+	$("#bluePercentageText").val($("#bluePercentage").val());
+	$("#emptyPercentageText").val($("#emptyPercentage").val());
+	$("#boardSizePercentageText").val($("#boardSizePercentage").val());
+	$("#delayTimeText").val($("#delayTime").val());
+
+	$("#similarPercentage").change(function(){
+		$("#similarPercentageText").val($("#similarPercentage").val());
+	});
+	$("#redPercentage").change(function(){
+		$("#redPercentageText").val($("#redPercentage").val());
+	});
+	$("#bluePercentage").change(function(){
+		$("#bluePercentageText").val($("#bluePercentage").val());
+	});
+	$("#emptyPercentage").change(function(){
+		$("#emptyPercentageText").val($("#emptyPercentage").val());
+	});
+	$("#boardSizePercentage").change(function(){
+		$("#boardSizePercentageText").val($("#boardSizePercentage").val());
+	});
+	$("#delayTime").change(function(){
+		$("#delayTimeText").val($("#delayTime").val());
+	});
 });
