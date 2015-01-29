a56902634e12086db1e18cd3b5bc64956c028463
zachrussell <zrussell1@harding.edu>
2015-01-29 10:39:15 -0600

    Added jQuery ui and a main js file

---
+++ js/main.js
@@ -0,0 +1,8 @@
+$(document).ready(function(){
+	$("#similarPercentage").slider();
+	$("#redPercentage").slider();
+	$("#bluePercentage").slider();
+	$("#emptyPercentage").slider();
+	$("#boardSizePercentage").slider();
+	$("#delayTime").slider();
+});
