3a5b0963803edf8b7db17685d76d87dc6d8543d5
zachrussell <zrussell1@harding.edu>
2015-02-04 17:56:27 -0600

    added board resize functionality and random color placement (w/ tansey)

--- js/main.js
+++ js/main.js
@@ -1,10 +1,62 @@
+
+var schelling = {
+	//variables
+
+
+
+
+
+
+	//methods
+	updateGameBoard: function(size){
+		$("#gameboard").html("");
+		for (var i = 0; i < size; i++) {
+			//make tr element
+			$("#gameboard").append("<tr id='row-" + i + "' class='row'></tr>")
+			for (var x = 0; x < size; x++) {
+				//make td elements
+				emptyNum = Math.floor(Math.random() * 100) + 1;
+
+				if(emptyNum <= $("#emptyPercentage").val()){
+					$("#row-" + i).append("<td id='pos-" + i + "-" + x +"' class='cell empty'>");
+				}
+				else{
+					colorRandom = Math.floor(Math.random() * 100) + 1;
+					if(colorRandom <= $("#redPercentage").val())
+						$("#row-" + i).append("<td id='pos-" + i + "-" + x +"' class='cell red'>");
+					else
+						$("#row-" + i).append("<td id='pos-" + i + "-" + x +"' class='cell blue'>");
+				}
+			};
+		};
+	},
+
+	startGame: function(){
+
+	},
+
+	stopGame: function(){
+
+	},
+
+	resetGame: function(){
+		//update board
+		schelling.updateGameBoard($("#boardSize").val());
+		//clear scores
+
+		//start game
+	}
+
+
+}
+
 $(document).ready(function(){
 
 	$("#similarPercentageText").val($("#similarPercentage").val());
 	$("#redPercentageText").val($("#redPercentage").val());
 	$("#bluePercentageText").val($("#bluePercentage").val());
 	$("#emptyPercentageText").val($("#emptyPercentage").val());
-	$("#boardSizePercentageText").val($("#boardSizePercentage").val());
+	$("#boardSizeText").val($("#boardSize").val());
 	$("#delayTimeText").val($("#delayTime").val());
 
 	$("#similarPercentage").on('input', function(){
@@ -19,10 +71,18 @@ $(document).ready(function(){
 	$("#emptyPercentage").on('input', function(){
 		$("#emptyPercentageText").val($("#emptyPercentage").val());
 	});
-	$("#boardSizePercentage").on('input', function(){
-		$("#boardSizePercentageText").val($("#boardSizePercentage").val());
+	$("#boardSize").on('input', function(){
+		$("#boardSizeText").val($("#boardSize").val());
 	});
 	$("#delayTime").on('input', function(){
 		$("#delayTimeText").val($("#delayTime").val());
 	});
+
+	schelling.updateGameBoard($("#boardSize").val());
+
+	$("#resetButton").click(function(){
+		schelling.resetGame();
+	});
+
+
 });
